0f181ac0d745631394653a68fc2198e1e98da29c
syf <yshi009@foxmail.com>
2026-05-02 15:47:24 +0800

    initial commit

--- src/zcnote_sphinx_theme/__init__.py
+++ src/zcnote_sphinx_theme/__init__.py
@@ -9,10 +9,20 @@ def get_html_theme_path():
 
 def setup(app):
     """Sphinx 扩展注册入口"""
-    theme_path = get_html_theme_path()
-    app.add_html_theme("zcnote_sphinx_theme", os.path.join(theme_path, "zcnote_sphinx_theme"))
+    # 1. 获取基础路径与当前主题的真实路径
+    base_theme_path = get_html_theme_path()
+    actual_theme_path = os.path.join(base_theme_path, "zcnote_sphinx_theme")
 
-    # 自动加载自定义样式，无需用户在 conf.py 中手动配置 html_css_files
+    # 2. 注册 HTML 主题
+    app.add_html_theme("zcnote_sphinx_theme", actual_theme_path)
+
+    # =========================================================
+    # 将 components 文件夹注入 Sphinx 的模板搜索路径
+    # =========================================================
+    components_path = os.path.join(actual_theme_path, "components")
+    app.config.templates_path.append(components_path)
+
+    # 3. 自动加载自定义样式，无需用户在 conf.py 中手动配置 html_css_files
     app.add_css_file("css/zcnote_custom.css")
 
     return {
