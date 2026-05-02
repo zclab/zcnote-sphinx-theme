9c1dea7b925807886b27e27544aecf9e4584cd5b
syf <yshi009@foxmail.com>
2026-05-02 08:45:58 +0800

    initial commit

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-# 【关键】将本地 src 目录加入 Python 路径，确保 Sphinx 能找到你正在开发的主题
-# 这样即使你不执行 pip install，也能直接读取到最新的 Python 逻辑
 sys.path.insert(0, os.path.abspath('../src'))
 
 project = 'ZCNote Theme Docs'
@@ -22,15 +20,21 @@ extensions = [
 myst_enable_extensions = ["colon_fence", "substitution"]
 myst_heading_anchors = 2
 
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 html_theme = 'zcnote_sphinx_theme'
+html_logo = "_static/logo.png"
+html_favicon = "_static/logo.png"
+html_sourcelink_suffix = ""
+html_last_updated_fmt = ""  # to reveal the build date in the pages meta
 
-# 测试我们的自定义选项以及继承自 pydata 的选项
+
 html_theme_options = {
-    "enable_collapse_sidebar": True,
+    "enable_collapse_sidebar": False,
     "navigation_depth": 4,
     "show_toc_level": 2,
     "search_as_you_type": True,
+    "use_edit_page_button": True,
     "icon_links": [
         {
             "name": "GitHub",
@@ -38,15 +42,22 @@ html_theme_options = {
             "icon": "fa-brands fa-github",
         },
     ],
+    "logo": {
+        "text": "ZCNote Theme",
+        "image_dark": "_static/logo.png",
+    },
     # "announcement": "https://raw.githubusercontent.com/pydata/pydata-sphinx-theme/main/docs/_templates/custom-template.html",
 }
 
-# 静态文件路径（文档级别的静态文件，非主题级别）
+html_context = {
+    "github_user": "zclab",
+    "github_repo": "zcnote-sphinx-theme",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+
+
 html_static_path = ['_static']
-
-# 覆盖默认的网页标题和侧边栏顶部标题（不再自动拼接 documentation）
 html_title = "ZCNote 主题文档"
-
-# （可选）如果你觉得 html_title 太长，还可以设置一个极简标题
-# html_short_title 通常会显示在左侧边栏顶部的 Logo 旁边
 html_short_title = "ZCNote"
+todo_include_todos = True
