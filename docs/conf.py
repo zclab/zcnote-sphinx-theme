7f84a3e6424f367548ac88b794750bafb96effa3
zc <yshi009@e.ntu.edu.sg>
2026-05-02 10:18:22 +0800

    initial commit (#3)
    Co-authored-by: syf <yshi009@foxmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx_design",
     "sphinx_copybutton",
+    "_extension.gallery_directive",
     "myst_parser",
     "sphinx_togglebutton",
 ]
@@ -46,6 +47,10 @@ html_theme_options = {
         "text": "ZCNote Theme",
         "image_dark": "_static/logo.png",
     },
+    "secondary_sidebar_items": {
+        "**/*": ["page-toc", "edit-this-page", "sourcelink"],
+        "examples/no-sidebar": [],
+    },
     # "announcement": "https://raw.githubusercontent.com/pydata/pydata-sphinx-theme/main/docs/_templates/custom-template.html",
 }
 
@@ -54,6 +59,11 @@ html_context = {
     "github_repo": "zcnote-sphinx-theme",
     "github_version": "main",
     "doc_path": "docs",
+}
+
+html_sidebars = {
+    "examples/no-sidebar": [],  # Test what page looks like with no sidebar items
+    "examples/persistent-search-field": ["search-field"],
 }
 
 
--- docs/examples/index.md
+++ docs/examples/index.md
@@ -1,12 +1,25 @@
-# 例子
+# Examples
 
-欢迎使用 ZCNote 主题测试页面。本页面用于验证主题的各项排版与交互功能。
+These sections show off various features and styles of this theme. They should help you understand how this theme behaves and how others are using it. See the sections in the primary sidebar and below to explore.
 
 ```{toctree}
 :maxdepth: 2
+:caption: Theme-specific styles
 
 test_layout
 math
 web-components
-admonitions
+kitchen-sink/index
+```
+
+
+```{toctree}
+:maxdepth: 2
+:caption: Reference and test section. Mostly meant for developers to check that things look OK.
+
+no-sidebar
+persistent-search-field
+in-page-toc
+mult_headers
+Link to an external site <https://jupyterbook.org/>
 ```
